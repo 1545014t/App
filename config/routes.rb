2880b0abc78ab9ad2dfa3c7918022fc4a95f9cdc
上森　香 <uemorikaoru@uemorikou-no-MacBook-Air.local>
2019-01-17 17:38:35 +0900

    users and microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
  get 'mainpage/home'
  get 'mainpage/sub'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
